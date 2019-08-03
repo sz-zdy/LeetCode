7439269d625e640a965a4b7476ac0bfadbba00af
ZDY <sznust@163.com>
2019-08-03 21:25:24 +0800

    add Solution of No.171 Problem (Easy)

--- Q171_ExcelSheetColumnNumber.cpp
+++ Q171_ExcelSheetColumnNumber.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
 #include <string>
+
 using namespace std;
 class Solution {
 public:
     int titleToNumber(string s) {
-        int result;
+        const int AlphabetNum = 26;
+        unsigned int result = 0;
+        for(char& ch:s){
+            result = result * AlphabetNum + ch - 'A' + 1;
+        }
         return result;
     }
 };
@@ -14,7 +18,7 @@ public:
 
 int main()
 {
-    string str = "";
+    string str = "AB";
     int result = Solution().titleToNumber(str);
     cout<< result << endl;
     return 0;
