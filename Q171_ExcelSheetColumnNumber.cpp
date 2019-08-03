6586222b12f7e5ecb99ea770addfefdedbccd2ec
ZDY <sznust@163.com>
2019-08-03 17:43:30 +0800

    test git too;

--- Q171_ExcelSheetColumnNumber.cpp
+++ Q171_ExcelSheetColumnNumber.cpp
@@ -1,11 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+using namespace std;
 class Solution {
 public:
     int titleToNumber(string s) {
-        
+        int result;
+        Solution().majorityElement()
+        return result;
     }
 };
 
+
 int main()
 {
+    string str = "";
+    int result = Solution().titleToNumber(str);
+    cout<< result << endl;
     return 0;
 }
