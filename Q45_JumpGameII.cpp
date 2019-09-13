3e7b8a2a48c8f0075ac161bb56dc262b7428a21f
SZ <sznust@163.com>
2019-09-13 11:35:00 +0800

    add Solution of No.452 Problem, tag: greedy strategy

--- Q45_JumpGameII.cpp
+++ Q45_JumpGameII.cpp
@@ -1,8 +1,8 @@
 /*
 * @Author: ShenZheng
 * @Date:   2019-04-05 15:24:59
-* @Last Modified by:   ShenZheng
-* @Last Modified time: 2019-04-05 15:59:11
+* @Last Modified by:   anchen
+* @Last Modified time: 2019-09-13 10:24:25
 */
 
 #include <iostream>
@@ -12,7 +12,22 @@ using namespace std;
 class Solution {
 public:
     int jump(vector<int>& nums) {
+        int start = 0;
+        int end = 0;
+        int result = 0;
+        while(end < nums.size()-1){
+            result++;
 
+            int temp = 0;       //本轮循环中的最远值
+            for(int i = start; i <= end; i++){
+                temp = max(temp,i+nums[i]);
+                if(temp >= nums.size()-1)
+                    break;
+            }
+            start = end + 1;
+            end = temp;
+        }
+        return result;
     }
 };
 
